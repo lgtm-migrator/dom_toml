4234eb72d5edd294149dd70679e98d7702243003
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-20 22:13:00 +0000

    Initial commit of code, tests and docs.

---
+++ tests/test_api.py
@@ -0,0 +1,272 @@
+# From https://github.com/uiri/toml
+#
+# The MIT License
+#
+# Copyright 2013-2019 William Pearson
+# Copyright 2015-2016 Julien Enselme
+# Copyright 2016 Google Inc.
+# Copyright 2017 Samuel Vasko
+# Copyright 2017 Nate Prewitt
+# Copyright 2017 Jack Evans
+# Copyright 2019 Filippo Broggini
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+# THE SOFTWARE.
+#
+
+# stdlib
+import copy
+import os
+import pathlib
+import sys
+from decimal import Decimal
+
+# 3rd party
+import pytest
+from toml import (
+		TomlArraySeparatorEncoder,
+		TomlDecoder,
+		TomlNumpyEncoder,
+		TomlPathlibEncoder,
+		TomlPreserveInlineDictEncoder
+		)
+from toml.decoder import InlineTableDict
+
+# this package
+from dom_toml import dump, dumps, load, loads
+
+test_toml = os.path.abspath(os.path.join(__file__, "..", "test.toml"))
+
+TEST_STR = """
+[a]\r
+b = 1\r
+c = 2
+"""
+
+TEST_DICT = {'a': {'b': 1, 'c': 2}}
+
+
+def test_bug_148():
+	assert 'a = "\\u0064"\n' == dumps({'a': "\\x64"})
+	assert 'a = "\\\\x64"\n' == dumps({'a': "\\\\x64"})
+	assert 'a = "\\\\\\u0064"\n' == dumps({'a': "\\\\\\x64"})
+
+
+def test_bug_144():
+	return
+
+	bug_dict = {"username": "×©×××"}
+	round_trip_bug_dict = loads(dumps(bug_dict))
+	unicoded_bug_dict = {"username": bug_dict["username"].decode("utf-8")}
+	assert round_trip_bug_dict == unicoded_bug_dict
+	assert bug_dict["username"] == (round_trip_bug_dict["username"].encode("utf-8"))
+
+
+def test_bug_196():
+	# stdlib
+	import datetime
+	d = datetime.datetime.now()
+	bug_dict = {'x': d}
+	round_trip_bug_dict = loads(dumps(bug_dict))
+	assert round_trip_bug_dict == bug_dict
+	assert round_trip_bug_dict['x'] == bug_dict['x']
+
+
+def test_circular_ref():
+	a = {}
+	b = {}
+	b['c'] = 4
+	b["self"] = b
+	a['b'] = b
+	with pytest.raises(ValueError):
+		dumps(a)
+
+	with pytest.raises(ValueError):
+		dumps(b)
+
+
+def test__dict():
+
+	class TestDict(dict):
+		pass
+
+	assert isinstance(loads(TEST_STR, dict_=TestDict), TestDict)
+
+
+def test_dict_decoder():
+
+	class TestDict(dict):
+		pass
+
+	test_dict_decoder = TomlDecoder(TestDict)
+	assert isinstance(loads(TEST_STR, decoder=test_dict_decoder), TestDict)
+
+
+def test_inline_dict():
+
+	class TestDict(dict, InlineTableDict):
+		pass
+
+	encoder = TomlPreserveInlineDictEncoder()
+	t = copy.deepcopy(TEST_DICT)
+	t['d'] = TestDict()
+	t['d']['x'] = "abc"
+	o = loads(dumps(t, encoder=encoder))
+	assert o == loads(dumps(o, encoder=encoder))
+
+
+def test_array_sep():
+	encoder = TomlArraySeparatorEncoder(separator=",\t")
+	d = {'a': [1, 2, 3]}
+	o = loads(dumps(d, encoder=encoder))
+	assert o == loads(dumps(o, encoder=encoder))
+
+
+def test_numpy_floats():
+	np = pytest.importorskip("numpy")
+
+	encoder = TomlNumpyEncoder()
+	d = {'a': np.array([1, .3], dtype=np.float64)}
+	o = loads(dumps(d, encoder=encoder))
+	assert o == loads(dumps(o, encoder=encoder))
+
+	d = {'a': np.array([1, .3], dtype=np.float32)}
+	o = loads(dumps(d, encoder=encoder))
+	assert o == loads(dumps(o, encoder=encoder))
+
+	d = {'a': np.array([1, .3], dtype=np.float16)}
+	o = loads(dumps(d, encoder=encoder))
+	assert o == loads(dumps(o, encoder=encoder))
+
+
+def test_numpy_ints():
+	np = pytest.importorskip("numpy")
+
+	encoder = TomlNumpyEncoder()
+	d = {'a': np.array([1, 3], dtype=np.int64)}
+	o = loads(dumps(d, encoder=encoder))
+	assert o == loads(dumps(o, encoder=encoder))
+
+	d = {'a': np.array([1, 3], dtype=np.int32)}
+	o = loads(dumps(d, encoder=encoder))
+	assert o == loads(dumps(o, encoder=encoder))
+
+	d = {'a': np.array([1, 3], dtype=np.int16)}
+	o = loads(dumps(d, encoder=encoder))
+	assert o == loads(dumps(o, encoder=encoder))
+
+
+def test_ordered():
+	# 3rd party
+	from toml import ordered as toml_ordered
+	encoder = toml_ordered.TomlOrderedEncoder()
+	decoder = toml_ordered.TomlOrderedDecoder()
+	o = loads(dumps(TEST_DICT, encoder=encoder), decoder=decoder)
+	assert o == loads(dumps(TEST_DICT, encoder=encoder), decoder=decoder)
+
+
+def test_tuple():
+	d = {'a': (3, 4)}
+	o = loads(dumps(d))
+	assert o == loads(dumps(o))
+
+
+def test_decimal():
+	PLACES = Decimal(10)**-4
+
+	d = {'a': Decimal("0.1")}
+	o = loads(dumps(d))
+	assert o == loads(dumps(o))
+	assert Decimal(o['a']).quantize(PLACES) == d['a'].quantize(PLACES)
+
+	with pytest.raises(TypeError):
+		loads(2)
+
+	with pytest.raises(TypeError, match="expected str, bytes or os.PathLike object, not int"):
+		load(2)
+
+	with pytest.raises(TypeError, match="expected str, bytes or os.PathLike object, not list"):
+		load([])
+
+	with pytest.raises(
+			TypeError,
+			match="argument should be a str object or an os.PathLike object returning str, not <class 'bytes'>"
+			):
+		load(b"test.toml")
+
+
+class FakeFile:
+
+	def __init__(self):
+		self.written = ''
+
+	def write(self, s):
+		self.written += s
+		return None
+
+	def read(self):
+		return self.written
+
+
+def test_dump(tmp_pathplus):
+	# stdlib
+	from collections import OrderedDict
+
+	dump(TEST_DICT, tmp_pathplus / "file.toml")
+	dump(load(tmp_pathplus / "file.toml", dict_=OrderedDict), tmp_pathplus / "file2.toml")
+	dump(load(tmp_pathplus / "file2.toml", dict_=OrderedDict), tmp_pathplus / "file3.toml")
+
+	assert (tmp_pathplus / "file2.toml").read_text() == (tmp_pathplus / "file3.toml").read_text()
+
+
+def test_paths():
+	load(test_toml)
+	load(pathlib.Path(test_toml))
+
+
+def test_nonexistent():
+	load(test_toml)
+
+	with pytest.raises(FileNotFoundError, match="No such file or directory: 'nonexist.toml'"):
+		load("nonexist.toml")
+
+
+def test_commutativity():
+	o = loads(dumps(TEST_DICT))
+	assert o == loads(dumps(o))
+
+
+def test_pathlib():
+	if (3, 4) <= sys.version_info:
+		# stdlib
+		import pathlib
+		o = {"root": {"path": pathlib.Path("/home/edgy")}}
+		test_str = """[root]
+path = "/home/edgy"
+"""
+		assert test_str == dumps(o, encoder=TomlPathlibEncoder())
+
+
+def test_deepcopy_timezone():
+	# stdlib
+	import copy
+
+	o = loads("dob = 1979-05-24T07:32:00-08:00")
+	o2 = copy.deepcopy(o)
+	assert o2["dob"] == o["dob"]
+	assert o2["dob"] is not o["dob"]
---
+++ tests/test_encoder.py
@@ -0,0 +1,62 @@
+# 3rd party
+import pytest
+from coincidence import AdvancedFileRegressionFixture
+
+# this package
+from dom_toml import TomlEncoder, dumps
+
+PEP621 = {
+		"name": "greppy",
+		"version": "0.0.0",
+		"description": "Recursively grep over Python files in the files in the given directory 🔎",
+		"readme": "README.rst",
+		"keywords": [],
+		"authors": [{"email": "dominic@davis-foster.co.uk", "name": "Dominic Davis-Foster"}],
+		"dynamic": ["requires-python", "classifiers", "dependencies"],
+		"license": {"file": "LICENSE"},
+		}
+
+array_of_tables = {"key": [
+		{"dict1": "dict1_value"},
+		{"dict2": "dict2_value"},
+		{"dict3": "dict3_value"},
+		]}
+
+
+@pytest.mark.parametrize(
+		"data",
+		[
+				pytest.param({"dotted.key": "string"}, id="dotted.key"),
+				pytest.param({"key": "string"}, id="string_value"),
+				pytest.param({"key": ["list"]}, id="list_value"),
+				pytest.param({
+						"key": [
+								"insure",
+								"auspicious",
+								"neglect",
+								"craven",
+								"match",
+								"worship",
+								"wave",
+								"languid",
+								"bad",
+								"news",
+								"flashy",
+								"recall",
+								"mother",
+								"festive",
+								"cup"
+								]
+						},
+								id="long_list"),
+				pytest.param({"key": ("list", )}, id="tuple_value"),
+				pytest.param({"key": {"dict": "dict_value"}}, id="dict_value"),
+				pytest.param(array_of_tables, id="array_of_tables"),
+				pytest.param({"section": {"key": "string"}}, id="section_string_value"),
+				pytest.param({"section": {"key": ["list"]}}, id="section_list_value"),
+				pytest.param({"section": {"key": ("list", )}}, id="section_tuple_value"),
+				pytest.param({"project": PEP621}, id="pep621"),
+				]
+		)
+def test_encoder(data, advanced_file_regression: AdvancedFileRegressionFixture):
+	advanced_file_regression.check(dumps(data, encoder=TomlEncoder(dict)), extension=".toml")
