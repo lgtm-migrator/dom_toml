2a537b0ef469882ce7efcf05be13506f1bce084e
Dominic Davis-Foster <dominic@davis-foster.co.uk>
2021-03-21 11:42:13 +0000

    Bump version v0.1.0 -> v0.1.1

--- dom_toml/__init__.py
+++ dom_toml/__init__.py
@@ -50,7 +50,7 @@ from toml.decoder import InlineTableDict
 __author__: str = "Dominic Davis-Foster"
 __copyright__: str = "2021 Dominic Davis-Foster"
 __license__: str = "MIT License"
-__version__: str = "0.1.0"
+__version__: str = "0.1.1"
 __email__: str = "dominic@davis-foster.co.uk"
 
 __all__ = ["TomlEncoder", "dumps", "loads", "dump", "load", "_M"]
